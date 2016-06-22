7472516d049053266c9df0b760cc3904762e0272
Aki Miyazaki <akimisaki3@gmail.com>
2016-06-22 22:23:36 +0900

    moved

--- src/com/akjava/gwt/threeammo/client/bones/PlainBoneCreator.java
+++ src/com/akjava/gwt/threeammo/client/bones/PlainBoneCreator.java
@@ -6,7 +6,10 @@ import com.akjava.gwt.three.client.gwt.boneanimation.AnimationBone;
 import com.akjava.gwt.three.client.java.utils.AnimationUtils;
 import com.akjava.gwt.three.client.js.THREE;
 import com.akjava.gwt.three.client.js.math.Vector3;
+import com.akjava.gwt.three.client.js.objects.Bone;
+import com.akjava.gwt.three.client.js.objects.SkinnedMesh;
 import com.akjava.gwt.threeammo.client.BodyAndMesh;
+import com.akjava.gwt.threeammo.client.ThreeAmmoControler;
 import com.akjava.gwt.threeammo.client.functions.BodyAndMeshFunctions;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.JsArray;
@@ -60,5 +63,40 @@ public class PlainBoneCreator {
 		
 		
 		return bones;
+	}
+	
+	public static void syncBones(ThreeAmmoControler ammoControler,SkinnedMesh mesh,int w,List<BodyAndMesh> particles){
+		
+		
+		JsArray<Bone> bones=mesh.getSkeleton().getBones();
+		
+		//LogUtils.log("reset!");
+		mesh.getSkeleton().pose();//is this reset?,maybe yes
+		
+		//no need [0] root
+		for(int i=1;i<bones.length();i++){
+			
+			if(bones.get(i).getName().equals("root")){
+				continue;
+			}
+			
+			String[] name=bones.get(i).getName().split(",");
+			int atX=Integer.parseInt(name[0]);
+			int atY=Integer.parseInt(name[1]);
+			
+			if(atY==0){ //has ?
+				continue;//locked;
+			}else{
+				//should i make a body and bone?
+				BodyAndMesh bm=getParticle(particles,w,atX, atY);
+				ammoControler.updateBone(bones.get(i), bm,null);
+			}	
+		}
+	}
+	
+	public static BodyAndMesh getParticle(List<BodyAndMesh> particles,int w,int x,int y){
+		int index=(w+1)*y+x;
+		//LogUtils.log(x+"x"+y+"="+index);
+		return particles.get(index);
 	}
 }
